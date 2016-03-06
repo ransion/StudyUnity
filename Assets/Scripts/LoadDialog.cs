5b4bced3ebe88d461bc8754d6b754c8d0cbf7367
Allen Kashiwa <358804906@qq.com>
2016-03-06 12:59:44 +0800

    finish load ugui dialog dynamically

---
+++ Assets/Scripts/LoadDialog.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadDialog : MonoBehaviour {
+	
+	void Start () {
+		GameObject UIRoot = GameObject.FindWithTag("UIRoot");
+		if(UIRoot != null)
+		{
+            Object dialogPrefab = Resources.Load("Prefabs/Dialog") as Object;
+			if(dialogPrefab != null)
+			{
+				GameObject dialog = Instantiate(dialogPrefab) as GameObject;
+				dialog.transform.SetParent(UIRoot.transform, false);
+                dialog.transform.SetAsLastSibling();
+            }
+            else
+            {
+                Debug.Log("Failed to load prefab file");
+            }
+		}else
+		{
+            Debug.LogError("There is not a GameObject with tag UIRoot");
+		}
+	}
+}
