d8d87ef42cbd9d96f795d785a86f7e0929c86351
Allen Kashiwa <358804906@qq.com>
2016-03-13 21:21:18 +0800

    build assetbungles

---
+++ Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public class BuildAssetBundles
+{
+
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/MyAssetBundles");
+    }
+}
