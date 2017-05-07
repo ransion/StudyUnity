ae7f6d5e0abbd49ca7be169f2ce8d799c552dfc9
Allen Kashiwa <358804906@qq.com>
2017-05-07 19:18:46 +0800

    初步验证正确性

---
+++ GizmosHelper/Assets/Scripts/CapsuleTest.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Utils.GizmosHelper;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+public class CapsuleTest : MonoBehaviour
+{
+
+    public Vector3 Center;
+    public float Radius;
+    public float Height;
+    public CapsuleDirection Direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GizmosHelper.Instance.DrawCapsule("myCapsule", transform.position, Center, transform.localScale, Radius, Height, Direction, Color.red);
+    }
+
+}
---
+++ GizmosHelper/Assets/Scripts/SphereTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Assets.Scripts.Utils.GizmosHelper;
+
+public class SphereTest : MonoBehaviour
+{
+    public Vector3 Center;
+    public float Radius;
+
+
+    void Update()
+    {
+        GizmosHelper.Instance.DrawSphere("mySphere", transform.position, Center, transform.localScale, Radius, Color.red);
+    }
+}
