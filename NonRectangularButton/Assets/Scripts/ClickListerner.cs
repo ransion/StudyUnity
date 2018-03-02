418db4d6d51e1648a27e67654088e74a77c796b0
Allen Kashiwa <358804906@qq.com>
2018-03-02 23:30:59 +0800

    add NonRectangularButton Project

---
+++ NonRectangularButton/Assets/Scripts/ClickListerner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickListerner : MonoBehaviour
+{
+    public Button leftButton;
+
+    public Button rightButton;
+
+    private void Start()
+    {
+        leftButton.onClick.AddListener(OnLeftButtonClick);
+        rightButton.onClick.AddListener(OnRightButtonClick);
+    }
+
+    private void OnRightButtonClick()
+    {
+        Debug.Log("Right Button Clicked");
+    }
+
+    private void OnLeftButtonClick()
+    {
+        Debug.Log("Left Button Clicked");
+    }
+}
